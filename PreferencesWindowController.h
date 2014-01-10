fe57ecf18b2b049a6bd8da40a93aef2b390e2a9b
Gaurav Chandrashekar <cggaurav@gmail.com>
2014-01-10 22:48:16 +0100

    Change Domain

---
+++ PreferencesWindowController.h
@@ -0,0 +1,15 @@
+//
+//  PreferencesWindowController.h
+//  PingMenu
+//
+//  Created by Gaurav Chandrashekar on 1/10/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PreferencesWindowController : NSWindowController
+@property (assign) IBOutlet NSTextField *domain;
+- (IBAction)buttonPressed:(id)sender;
+
+@end
---
+++ PreferencesWindowController.m
@@ -0,0 +1,44 @@
+//
+//  PreferencesWindowController.m
+//  PingMenu
+//
+//  Created by Gaurav Chandrashekar on 1/10/14.
+//
+//
+
+#import "PreferencesWindowController.h"
+#import "PingMenuAppDelegate.h"
+
+@interface PreferencesWindowController ()
+
+@end
+
+@implementation PreferencesWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    PingMenuAppDelegate *AppDelegate = (PingMenuAppDelegate *)[[NSApplication sharedApplication] delegate];
+    self.domain.stringValue = AppDelegate.PING_HOST;
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+- (IBAction)buttonPressed:(id)sender {
+    NSLog(@"%@", self.domain.stringValue);
+    
+    PingMenuAppDelegate *AppDelegate = (PingMenuAppDelegate *)[[NSApplication sharedApplication] delegate];
+    AppDelegate.PING_HOST = self.domain.stringValue;
+    self.close;
+}
+@end
